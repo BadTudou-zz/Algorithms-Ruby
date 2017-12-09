2a61df210f194ba91072f337b18486808633621a
BadTudou <badtudou@gmail.com>
2017-12-09 11:23:33 +0800

    重构【堆栈】

--- Stack.rb
+++ Stack.rb
@@ -7,22 +7,35 @@ require 'LinkedList'
 class Stack
   attr_accessor :header, :footer
 
-  def initialize(val)
-    @header = @footer = LinkedList.new(val)
+  def initialize()
+    @header = @footer = nil
   end
 
   # 入栈
-  def push(val)
-    @footer = @footer.add(val)
+  def push(val=nil)
+    return self if val.nil?
+
+    if @footer.nil?
+      @header = @footer = LinkedList.new(val)
+    else
+      @footer = @footer.add(val)
+    end
+
     self
   end
 
   # 出栈
   def pop
-    return nil if @footer.nil?
-    val = @footer.val
-    @footer = @footer.pre
-    @footer.next = nil
+    if @header.nil?
+      val = nil
+    elsif @header == @footer
+      val = @header.val
+      @header = @footer = nil
+    else
+      val = @footer.val
+      @footer = @footer.pre
+      @footer.next = nil
+    end
     val
   end
 
@@ -35,8 +48,18 @@ class Stack
     end
   end
 
+  # 栈顶
+  def top
+    return self.empty? ? nil : @footer.val
+  end
+
+  # 栈底
+  def bottom
+    return self.empty? ? nil : @header.val
+  end
+
   # 是否为空
   def empty?
-    return self.header == self.footer
+    return self.header.nil?
   end
 end
